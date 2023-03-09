7197d57743f846ca6d97fd18a58e442542a32ef3
ednacoveja <ovejerocande@gmail.com>
2023-03-09 19:56:35 -0300

    limpiando

--- js/script.js
+++ js/script.js
@@ -7,27 +7,21 @@ $(function () {
 });
 
 $(document).ready(function () {
-  // AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
   $('nav__button .button[category="all"]').addClass("ct_item-active");
-
-  // FILTRANDO PRODUCTOS  ============================================
 
   $(".button").click(function () {
     var catProduct = $(this).attr("category");
     console.log(catProduct);
 
-    // AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
     $(".button").removeClass("ct_item-active");
     $(this).addClass("ct_item-active");
 
-    // OCULTANDO PRODUCTOS =========================
     $(".product-item").css("transform", "scale(0)");
     function hideProduct() {
       $(".product-item").hide();
     }
     setTimeout(hideProduct, 400);
 
-    // MOSTRANDO PRODUCTOS =========================
     function showProduct() {
       $('.product-item[category="' + catProduct + '"]').show();
       $('.product-item[category="' + catProduct + '"]').css(
@@ -48,4 +42,3 @@ $(document).ready(function () {
     setTimeout(showAll, 400);
   });
 });
-
